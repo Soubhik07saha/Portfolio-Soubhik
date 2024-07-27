49aa87143dab9351ed87a5149b2e6c5047f70b9e
Soubhik07saha <162893486+Soubhik07saha@users.noreply.github.com>
2024-07-27 06:25:50 -0700

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose");
 
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        method: ["POST", "GET"],
+        credentials: true
+    }
+)); 
+
 const conn = async() => {
     try{
         await mongoose.connect("mongodb+srv://soubhiksahaetce222711:Soubhik@123@cluster0.nw2jtsc.mongodb.net/portfolioContact").then(() => {
